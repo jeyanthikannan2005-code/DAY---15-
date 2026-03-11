7b5216cdf975c9c061a7585097150930a9b991c4
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-03-11 11:57:33 +0530

    To print all even numbers between 1 and 50

---
+++ 11-q2/EvenNum.java
@@ -0,0 +1,14 @@
+class EvenNum
+{
+	public static void main(String[] args)
+	{
+		System.out.println("EVEN NUMBERS FROM 1 TO 50 ARE ");
+		for(int i=1;i<=50;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
